78fe1f83641d9cd40e30a95c3255baf007ec6326
SC-80414 <SC-80414@MA-L4B5R48JRJCN>
2024-12-27 12:19:39 +0100

    Meme Generator - Fetch Memes

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 
 
@@ -9,6 +9,15 @@ export default function Main() {
         bottomText: "you forgot to save your code",
         imageUrl:  "http://i.imgflip.com/1bij.jpg"
       })
+
+      const [allMemes,setAllMemes]= useState([])
+
+      useEffect(()=>{
+        fetch( "https://api.imgflip.com/get_memes")
+        .then(response => response.json())
+        .then(data=>setAllMemes(data.data.memes))
+        
+    },[])
 
       function handleChange(event){
         const {value, name}=event.currentTarget
